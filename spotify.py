8b91f1a49122ef48023f61252910ede8ead4639d
Harin <master.harin@gmail.com>
2021-05-24 14:01:33 -0700

    ansible set up!

--- spotify.py
+++ spotify.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return 'Welcome to Spotify, Harin & Anna!'
+    return 'Welcome to SpotifyPro, Harin & Anna!'
 
 @app.route("/api/healthcheck")
 def healthcheck():
     return jsonify(status='success')
+
+@app.route("/api/moveSong")
+def welcome():
+    return 'This song will be properly classified!'
